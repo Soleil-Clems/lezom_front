bf8b9c4d4891e8346c21d5262a5bd90bd1180dae
cassidy-epitech <cassidy1.nguyen@epitech.eu>
2026-01-28 15:12:49 +0100

    separate Express app from server bootstrap for testability

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+const adminRoutes = require("./routes/admin.routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/api-docs.json", (_req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+
+app.get("/health", (_req, res) => {
+  res.json({ status: "ok" });
+});
+
+module.exports = app;
