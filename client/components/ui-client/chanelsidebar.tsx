6656c44415d342e9e4c831ccf7b609889bdbfc56
Alexandre <alexandre.crapanzano@epitech.eu>
2026-02-02 10:04:45 +0100

    add dropdownmenu

--- client/components/ui-client/chanelsidebar.tsx
+++ client/components/ui-client/chanelsidebar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Hash, UserPlus, Volume2 } from "lucide-react"; 
 import { MOCK_CHANNELS } from "@/lib/mock-data";
 import { SidebarMenuButton } from "../ui/sidebar";
+import { ServerSettingsDropdown } from "./dropdownMenu";
 type ChannelSidebarProps = {
   serverId?: string; 
   channelId?: string;
@@ -20,38 +21,44 @@ export function ChannelSidebar({ serverId, channelId }: ChannelSidebarProps) {
 
   const channels = MOCK_CHANNELS[serverId] || [];
   
+  console.log()
 
   return (
     <div className="w-full md:w-60 h-full bg-[#2B2D31] flex flex-col shrink-0 border-r border-black/20">
-      <div className="h-12 px-4 flex items-center shadow-sm border-b border-black/20 font-bold text-white shrink-0">
+    <div className="h-12 px-4 flex items-center justify-between shadow-sm border-b border-black/20 font-bold text-white shrink-0">
         Salons
+        <div className="flex items-center">
+        <ServerSettingsDropdown />
+      </div>
       </div>
       
       
       <div className="flex-1 overflow-y-auto p-2 space-y-[2px]">
         {channels.map((channel) => {
           const isActive = channel.id === channelId;
+          
 
           return (
             <Link 
               key={channel.id}
-              href={`/app/(root)/servers/${serverId}/${channel.id}`}
+              href={`/servers/${serverId}/${channel.id}`}   
               className={`
                 flex items-center gap-2 px-2 py-1.5 rounded-md transition-colors group
                 ${isActive 
-                  ? "bg-zinc-700/60 text-white" 
-                  : "text-zinc-400 hover:bg-zinc-700/30 hover:text-zinc-200"}
-              `}
+          ? "bg-zinc-700/60 text-white rounded-md" 
+          : "text-zinc-400 hover:bg-zinc-700/30 hover:text-zinc-200 rounded-md"}
+      `}
+              
             >
               {channel.type === "text" ? (
-                <Hash className="w-5 h-5 text-zinc-500 shrink-0" />
-              ) : (
-                <Volume2 className="w-5 h-5 text-zinc-500 shrink-0" />
-              )}
+        <Hash className={`w-5 h-5 shrink-0 ${isActive ? "text-zinc-200" : "text-zinc-500"}`} />
+          ) : (
+        <Volume2 className={`w-5 h-5 shrink-0 ${isActive ? "text-zinc-200" : "text-zinc-500"}`} />
+          )}
 
-              <span className="truncate font-medium">
-                {channel.name}
-              </span>
+              <span className={`truncate font-medium ${isActive ? "text-white" : ""}`}>
+        {channel.name}
+      </span>
             </Link>
           );
         })}
