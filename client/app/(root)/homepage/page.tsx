5ba954bbb9e9f44a993da2b183c196ee13b6e8cd
Alexandre <alexandre.crapanzano@epitech.eu>
2026-02-02 16:39:00 +0100

    friendlist

--- client/app/(root)/homepage/page.tsx
+++ client/app/(root)/homepage/page.tsx
@@ -1,34 +1,29 @@
 import { ChannelSidebar } from "@/components/ui-client/chanelsidebar";
 import MessageLayout from "@/components/ui-client/MessageLayout";
-import { ServerItem } from "@/components/ui-client/serverItem";
+import { OnlineFriendsList } from "@/components/ui-client/onlinefriendlist";
 import { ServerSidebar } from "@/components/ui-client/serversidebar";
-import { MOCK_SERVERS } from "@/lib/mock-data";
-
 
 export default function HomePage() {
-  console.log(MOCK_SERVERS);
-
   return (
-
-    <div className="flex h-screen w-full overflow-hidden">
-      
-      <ServerSidebar>
-        {MOCK_SERVERS.map((server) => (
-          <ServerItem 
-            key={server.id} 
-            id={server.id} 
-            name={server.name} 
-            image={server.image} 
-          />
-        ))}
-      </ServerSidebar>
-
+    <div className="flex h-screen w-full overflow-hidden bg-[#313338]">
+      <ServerSidebar />
       <ChannelSidebar />
       
-      <div className="flex-1">
-        <MessageLayout />
-      </div>
-      
+      {/* On force une bordure rouge sur le main et bleue sur l'aside 
+          pour voir qui prend quelle place 
+      */}
+      <main className="flex-1 flex min-w-0 border-4 border-red-500">
+        
+        <div className="flex-1">
+           <MessageLayout />
+        </div>
+
+        <aside className="w-80 bg-[#2B2D31] border-4 border-blue-500 flex flex-col shrink-0">
+          <div className="p-4 text-white">TEST ASIDE VISIBLE</div>
+          <OnlineFriendsList />
+        </aside>
+        
+      </main>
     </div>
   )
 }
