109033db4fa14f99fb2c9135d39090d59e14c629
Soleil-Clems <ouisolclems@gmail.com>
2026-02-05 00:42:52 +0100

    Feat: changing socket server by hook

--- client/components/ui-client/MessageLayout.tsx
+++ client/components/ui-client/MessageLayout.tsx
@@ -2,24 +2,24 @@
 import MessageScreenComponent from './MessageScreenComponent'
 import Message from '@/components/ui-client/messageComponent'
 import Loading from "@/components/ui-client/Loading";
-import { useSocketMessages } from "@/hooks/websocket/useSocketMessages";
+import {useSocketMessages} from "@/hooks/websocket/useSocketMessages";
 
-export default function MessageLayout({ channelId }: { channelId: string }) {
-    const { messages, isLoading, typingUsers } = useSocketMessages(channelId);
+export default function MessageLayout({channelId}: { channelId: string }) {
+    const {messages, isLoading, typingUsers} = useSocketMessages(channelId);
 
-    if (isLoading) return <Loading />;
+    if (isLoading) return <Loading/>;
 
     return (
         <div className='flex flex-col w-full h-full overflow-hidden bg-[#313338]'>
             <div className="flex-1 overflow-y-auto">
-                {/* On passe typingUsers au composant d'affichage */}
+
                 <MessageScreenComponent
                     messages={messages}
                     typingUsers={typingUsers}
                 />
             </div>
             <div className="shrink-0">
-                <Message channelId={channelId} />
+                <Message channelId={channelId}/>
             </div>
         </div>
     );
