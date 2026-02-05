cadc0914b37f208febbd85d82bd490f76832b626
Th3-ripper <adonai-clems-soleil.ouisol@epitech.eu>
2026-02-05 16:43:31 +0100

    Fix: added file upload

--- client/enums/enum.ts
+++ client/enums/enum.ts
@@ -1,3 +1,4 @@
-import { z } from "zod";
+import {z} from "zod";
+
 export const ChannelTypeEnum = z.enum(["call", "text"]);
-export const MessageTypeEnum = z.enum(["img", "text", "file", "pdf", "system", "gif"]);
+export const MessageTypeEnum = z.enum(["img", "text", "file", "pdf", "system", "gif", "voice"]);
