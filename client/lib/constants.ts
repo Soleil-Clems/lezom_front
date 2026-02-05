5454fd0af9f7de6e17a88efc71f1851d5c65880d
Th3-ripper <adonai-clems-soleil.ouisol@epitech.eu>
2026-02-05 10:00:37 +0100

    Feat: Add Gift api key

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,2 +1,3 @@
-export const gifApiKey: string = process.env.RTENOR_API_KEY || "";
+export const gifApiKey: string  = process.env.NEXT_PUBLIC_TENOR_API_KEY || '' ;
+export const gifClientKey: string  = process.env.NEXT_PUBLIC_TENOR_CLIENT_KEY || '' ;
 
